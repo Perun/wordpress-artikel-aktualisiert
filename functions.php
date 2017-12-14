488be608b0f5fa7e964e71ec62423e3c92261584
Vladimir Simović <info@perun.net>
2017-12-14 04:21:39 +0100

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+// Artikel-Aktualisierung anzeigen
+function aktualisierung()
+{
+    $artikel_erstellt = get_the_date('U');
+    $artikel_aktualisiert = get_post_modified_time('U');
+
+    if (($artikel_aktualisiert - $artikel_erstellt ) > 43200) {
+        echo ' &ndash; Aktualisiert: ' . get_the_modified_date('d. F Y');
+    }
+}
